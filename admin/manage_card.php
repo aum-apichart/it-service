96ef3c70d2812b91af330d3271ed310d5d3e6c27
aum-apichart <aapichart52@gmail.com>
2025-02-05 12:07:04 +0700

    Update manage_card.php

--- admin/manage_card.php
+++ admin/manage_card.php
@@ -52,6 +52,7 @@ $result = $conn->query($sql);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <h1>สวัสดี</h1>
 <div class="container mt-5">
     <h1 class="mb-4">Manage Cards</h1>
 
